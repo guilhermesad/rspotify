e8873674b29d033030f85d4590cce8b883946af8
GuilhermeSad <gorgulhoguilherme@gmail.com>
2014-06-20 16:09:14 -0300

    initing rspec

---
+++ spec/lib/rspotify_spec.rb
@@ -0,0 +1,2 @@
+describe RSpotify do
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+require 'rspotify'
+
+RSpec.configure do |config|
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 10
+
+  config.order = :random
+
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+
+    mocks.verify_partial_doubles = true
+  end
+end
