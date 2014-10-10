e5c0115e133ad2b54b4b56e25edcade13b945570
GuilhermeSad <gorgulhoguilherme@gmail.com>
2014-10-10 20:05:57 +0200

    fixing rspec for User#playlists

--- spec/lib/rspotify/user_spec.rb
+++ spec/lib/rspotify/user_spec.rb
@@ -21,12 +21,11 @@ describe RSpotify::User do
       # Keys generated specifically for the tests. Should be removed in the future
       client_id     = '5ac1cda2ad354aeaa1ad2693d33bb98c'
       client_secret = '155fc038a85840679b55a1822ef36b9b'
-      VCR.use_cassette('authenticate:5ac1cda2ad354aeaa1ad2693d33bb98c') do 
+      VCR.use_cassette('authenticate:5ac1cda2ad354aeaa1ad2693d33bb98c') do
         RSpotify.authenticate(client_id, client_secret)
       end
 
-
-      playlists = VCR.use_cassette('user:wizzler:playlists') do 
+      playlists = VCR.use_cassette('user:wizzler:playlists:limit:20:offset:0') do
         @user.playlists
       end
       expect(playlists)             .to be_an Array
