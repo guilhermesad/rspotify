c6321c6434c94eb9e04ee623c6bf014011d4cd53
GuilhermeSad <gorgulhoguilherme@gmail.com>
2014-11-10 22:49:12 +0100

    refactoring: all connection methods are from eigenclass

--- lib/rspotify/connection.rb
+++ lib/rspotify/connection.rb
@@ -9,45 +9,48 @@ module RSpotify
   TOKEN_URI     = 'https://accounts.spotify.com/api/token'
   VERBS         = %w(get post put delete)
 
-  def self.auth_header
-    authorization = Base64.strict_encode64 "#{@client_id}:#{@client_secret}"
-    { 'Authorization' => "Basic #{authorization}" }
-  end
-  private_class_method :auth_header
+  class << self
 
-  # Authenticates access to restricted data. Requires {https://developer.spotify.com/my-applications user credentials}
-  #
-  # @param client_id [String]
-  # @param client_secret [String]
-  #
-  # @example
-  #           RSpotify.authenticate("<your_client_id>", "<your_client_secret>")
-  #
-  #           playlist = RSpotify::Playlist.find('wizzler', '00wHcTN0zQiun4xri9pmvX')
-  #           playlist.name #=> "Movie Soundtrack Masterpieces"
-  def self.authenticate(client_id, client_secret)
-    @client_id, @client_secret = client_id, client_secret
-    request_body = { grant_type: 'client_credentials' }
-    response = RestClient.post(TOKEN_URI, request_body, auth_header)
-    @client_token = JSON.parse(response)['access_token']
-    true
-  end
-
-  def self.api_url(path)
-    path.start_with?("http") ? path : API_URI + path
-  end
-  private_class_method :api_url
-
-  VERBS.each do |verb|
-    define_singleton_method verb do |path, *params|
-      response = RestClient.send(verb, api_url(path), *params)
-      JSON.parse response unless response.empty?
+    # Authenticates access to restricted data. Requires {https://developer.spotify.com/my-applications user credentials}
+    #
+    # @param client_id [String]
+    # @param client_secret [String]
+    #
+    # @example
+    #           RSpotify.authenticate("<your_client_id>", "<your_client_secret>")
+    #
+    #           playlist = RSpotify::Playlist.find('wizzler', '00wHcTN0zQiun4xri9pmvX')
+    #           playlist.name #=> "Movie Soundtrack Masterpieces"
+    def authenticate(client_id, client_secret)
+      @client_id, @client_secret = client_id, client_secret
+      request_body = { grant_type: 'client_credentials' }
+      response = RestClient.post(TOKEN_URI, request_body, auth_header)
+      @client_token = JSON.parse(response)['access_token']
+      true
     end
 
-    define_singleton_method "auth_#{verb}" do |path, *params|
-      auth_header = { 'Authorization' => "Bearer #{@client_token}" }
-      params << auth_header
-      send(verb, path, *params)
+    VERBS.each do |verb|
+      define_method verb do |path, *params|
+        response = RestClient.send(verb, api_url(path), *params)
+        JSON.parse response unless response.empty?
+      end
+
+      define_method "auth_#{verb}" do |path, *params|
+        auth_header = { 'Authorization' => "Bearer #{@client_token}" }
+        params << auth_header
+        send(verb, path, *params)
+      end
+    end
+
+    private
+
+    def api_url(path)
+      path.start_with?("http") ? path : API_URI + path
+    end
+
+    def auth_header
+      authorization = Base64.strict_encode64 "#{@client_id}:#{@client_secret}"
+      { 'Authorization' => "Basic #{authorization}" }
     end
   end
 end
