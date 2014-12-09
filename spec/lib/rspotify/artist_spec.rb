aa2df04525e7ae60201726ea174a4b7faa3e3d28
Gustavo Matias <gustavo@matias.com>
2014-12-08 22:43:54 -0500

    Artist now has followers

--- spec/lib/rspotify/artist_spec.rb
+++ spec/lib/rspotify/artist_spec.rb
@@ -11,7 +11,8 @@ describe RSpotify::Artist do
 
     it 'should find artist with correct attributes' do
       expect(@artist.external_urls['spotify']) .to eq      'https://open.spotify.com/artist/7Ln80lUS6He07XvHI8qqHH'
-      expect(@artist.genres)                   .to include 'Alternative Pop/Rock', 'Alternative/Indie Rock', 'Indie', 'Indie Rock', 'Pop/Rock'
+      expect(@artist.followers['total'])       .to be      > 0
+      expect(@artist.genres)                   .to be_an   Array
       expect(@artist.href)                     .to eq      'https://api.spotify.com/v1/artists/7Ln80lUS6He07XvHI8qqHH'
       expect(@artist.id)                       .to eq      '7Ln80lUS6He07XvHI8qqHH'
       expect(@artist.images)                   .to include ({'height' => 1333, 'width' => 1000, 'url' => 'https://i.scdn.co/image/fa2e9ca1a27695ae7f8013350d9a53e11d523ece'})
