fe75af5587b80c2257698595034101fecdd5ca25
leefreemanxyz <lee.freeman1@gmail.com>
2017-11-24 22:20:57 +0100

    attempting chaining exceptions

--- lib/rspotify/connection.rb
+++ lib/rspotify/connection.rb
@@ -60,7 +60,7 @@ module RSpotify
 
       begin
         response = RestClient.send(verb, url, *params)
-      rescue RestClient::Forbidden
+      rescue RestClient::Forbidden, RestClient::Unauthorized
         if @client_token
           authenticate(@client_id, @client_secret)
 
