0b3952d6ed93eae67d8a6209e990acfc50401f43
Deuteu <Deuteu@users.noreply.github.com>
2018-10-11 23:26:25 +0200

    Apply syntax fix to User

--- lib/rspotify/user.rb
+++ lib/rspotify/user.rb
@@ -1,5 +1,4 @@
 module RSpotify
-
   # @attr [String] birthdate       The user's date-of-birth. This field is only available when the current user has granted access to the *user-read-birthdate* scope.
   # @attr [String] country         The country of the user, as set in the user's account profile. An {http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2 ISO 3166-1 alpha-2 country code}. This field is only available when the current user has granted access to the *user-read-private* scope.
   # @attr [Hash]   credentials     The credentials generated for the user with OAuth. Includes access token, token type, token expiration time and refresh token. This field is only available when the current user has granted access to any scope.
@@ -10,7 +9,6 @@ module RSpotify
   # @attr [String] product         The user's Spotify subscription level: "premium", "free", etc. This field is only available when the current user has granted access to the *user-read-private* scope.
   # @attr [Hash]   tracks_added_at A hash containing the date and time each track was saved by the user. Note: the hash is filled and updated only when {#saved_tracks} is used.
   class User < Base
-
     # Returns User object with id provided
     #
     # @param id [String]
@@ -44,7 +42,7 @@ module RSpotify
     private_class_method :refresh_token
 
     def self.extract_custom_headers(params)
-      headers_param = params.find{|x| x.is_a?(Hash) && x[:headers]}
+      headers_param = params.find { |x| x.is_a?(Hash) && x[:headers] }
       params.delete(headers_param) if headers_param
       headers_param ? headers_param[:headers] : {}
     end
@@ -63,9 +61,9 @@ module RSpotify
       headers = oauth_header(user_id).merge(custom_headers)
       params << headers
       RSpotify.send(:send_request, verb, path, *params)
-
     rescue RestClient::Exception => e
       raise e if e.response !~ /access token expired/
+
       refresh_token(user_id)
       params[-1] = oauth_header(user_id).merge(custom_headers)
       RSpotify.send(:send_request, verb, path, *params)
@@ -94,11 +92,11 @@ module RSpotify
 
       super(options)
 
-      if credentials
-        @@users_credentials ||= {}
-        @@users_credentials[@id] = credentials
-        @credentials = @@users_credentials[@id]
-      end
+      return unless credentials
+
+      @@users_credentials ||= {}
+      @@users_credentials[@id] = credentials
+      @credentials = @@users_credentials[@id]
     end
 
     # Creates a playlist in user's Spotify account. This method is only available when the current
@@ -118,25 +116,28 @@ module RSpotify
     #           playlist.public #=> false
     def create_playlist!(name, public: true)
       url = "users/#{@id}/playlists"
-      request_data = { name: name, public: public }.to_json
-
+      request_data = {name: name, public: public}.to_json
       response = User.oauth_post(@id, url, request_data)
+
       return response if RSpotify.raw_response
+
       Playlist.new response
     end
 
-    # Get the current user’s player
+    # Get the current user's player
     #
     # @example
     #           player = user.player
     def player
-      url = "me/player"
+      url = 'me/player'
       response = User.oauth_get(@id, url)
+
       return response if RSpotify.raw_response
+
       response.present? ? Player.new(self, response) : nil
     end
 
-    # Get the current user’s recently played tracks. Requires the *user-read-recently-played* scope.
+    # Get the current user's recently played tracks. Requires the *user-read-recently-played* scope.
     #
     # @param limit  [Integer] Optional. The number of entities to return. Default: 20. Minimum: 1. Maximum: 50.
     # @return [Array<Track>]
@@ -148,6 +149,7 @@ module RSpotify
     def recently_played(limit: 20)
       url = "me/player/recently-played?limit=#{limit}"
       response = RSpotify.resolve_auth_request(@id, url)
+
       return response if RSpotify.raw_response
 
       json = RSpotify.raw_response ? JSON.parse(response) : response
@@ -175,16 +177,14 @@ module RSpotify
     #           playlist = RSpotify::Playlist.search('Movie').first
     #           user.follow(playlist, public: false)
     def follow(followed, public: true)
-      if followed.is_a? Array
-        ids = followed.map(&:id).join(',')
-        type = followed.first.type
-      else
-        ids = followed.id
-        type = followed.type
-      end
+      ids, type = if followed.is_a?(Array)
+                    [followed.map(&:id).join(','), followed.first.type]
+                  else
+                    [followed.id, followed.type]
+                  end
 
       if type == 'playlist'
-        request_body = { public: public }
+        request_body = {public: public}
         url = "users/#{followed.owner.id}/playlists/#{followed.id}/followers"
       else
         request_body = {}
@@ -195,7 +195,7 @@ module RSpotify
       followed
     end
 
-    # Get the current user’s followed artists or users. Requires the *user-follow-read* scope.
+    # Get the current user's followed artists or users. Requires the *user-follow-read* scope.
     #
     # @note The current Spotify API implementation only supports getting followed *artists*
     #
@@ -213,9 +213,10 @@ module RSpotify
       type_class = RSpotify.const_get(type.capitalize)
       url = "me/following?type=#{type}&limit=#{limit}"
       url << "&after=#{after}" if after
-
       response = User.oauth_get(@id, url)
+
       return response if RSpotify.raw_response
+
       response["#{type}s"]['items'].compact.map { |i| type_class.new i }
     end
 
@@ -255,11 +256,13 @@ module RSpotify
     def playlists(limit: 20, offset: 0)
       url = "users/#{@id}/playlists?limit=#{limit}&offset=#{offset}"
       response = RSpotify.resolve_auth_request(@id, url)
+
       return response if RSpotify.raw_response
+
       response['items'].map { |i| Playlist.new i }
     end
 
-    # Remove tracks from the user’s “Your Music” library.
+    # Remove tracks from the user's "Your Music" library.
     #
     # @param tracks [Array<Track>] The tracks to remove. Maximum: 50.
     # @return [Array<Track>] The tracks removed.
@@ -277,7 +280,7 @@ module RSpotify
       tracks
     end
 
-    # Save tracks to the user’s “Your Music” library.
+    # Save tracks to the user's "Your Music" library.
     #
     # @param tracks [Array<Track>] The tracks to save. Maximum: 100.
     # @return [Array<Track>] The tracks saved.
@@ -290,13 +293,13 @@ module RSpotify
     #           user.saved_tracks.size #=> 20
     def save_tracks!(tracks)
       tracks_ids = tracks.map(&:id)
-      url = "me/tracks"
+      url = 'me/tracks'
       request_body = tracks_ids.inspect
       User.oauth_put(@id, url, request_body)
       tracks
     end
 
-    # Returns the tracks saved in the Spotify user’s “Your Music” library
+    # Returns the tracks saved in the Spotify user's "Your Music" library
     #
     # @param limit  [Integer] Maximum number of tracks to return. Maximum: 50. Minimum: 1. Default: 20.
     # @param offset [Integer] The index of the first track to return. Use with limit to get the next set of tracks. Default: 0.
@@ -317,10 +320,11 @@ module RSpotify
       end
 
       return response if RSpotify.raw_response
+
       tracks.map { |t| Track.new t['track'] }
     end
 
-    # Check if tracks are already saved in the Spotify user’s “Your Music” library
+    # Check if tracks are already saved in the Spotify user's "Your Music" library
     #
     # @param tracks [Array<Track>] The tracks to check. Maximum: 50.
     # @return [Array<Boolean>] Array of booleans, in the same order in which the tracks were specified.
@@ -334,7 +338,7 @@ module RSpotify
       User.oauth_get(@id, url)
     end
 
-    # Remove albums from the user’s “Your Music” library.
+    # Remove albums from the user's "Your Music" library.
     #
     # @param albums [Array<Album>] The albums to remove. Maximum: 50.
     # @return [Array<Album>] The albums removed.
@@ -352,7 +356,7 @@ module RSpotify
       albums
     end
 
-    # Save albums to the user’s “Your Music” library.
+    # Save albums to the user's "Your Music" library.
     #
     # @param albums [Array<Album>] The albums to save. Maximum: 50.
     # @return [Array<Album>] The albums saved.
@@ -365,13 +369,13 @@ module RSpotify
     #           user.saved_albums.size #=> 10
     def save_albums!(albums)
       albums_ids = albums.map(&:id)
-      url = "me/albums"
+      url = 'me/albums'
       request_body = albums_ids.inspect
       User.oauth_put(@id, url, request_body)
       albums
     end
 
-    # Returns the albums saved in the Spotify user’s “Your Music” library. ** Includes albums whose tracks you saved
+    # Returns the albums saved in the Spotify user's "Your Music" library. ** Includes albums whose tracks you saved
     #
     # @param limit  [Integer] Maximum number of albums to return. Maximum: 50. Minimum: 1. Default: 20.
     # @param offset [Integer] The index of the first album to return. Use with limit to get the next set of albums. Default: 0.
@@ -389,10 +393,11 @@ module RSpotify
       albums = json['items'].select { |i| i['album'] }
 
       return response if RSpotify.raw_response
+
       albums.map { |a| Album.new a['album'] }
     end
 
-    # Check if albums are already saved in the Spotify user’s “Your Music” library. ** Only returns true if the album was saved via me/albums, not if you saved each track individually.
+    # Check if albums are already saved in the Spotify user's "Your Music" library. ** Only returns true if the album was saved via me/albums, not if you saved each track individually.
     #
     # @param albums [Array<Album>] The albums to check. Maximum: 50.
     # @return [Array<Boolean>] Array of booleans, in the same order in which the albums were specified.
@@ -414,7 +419,7 @@ module RSpotify
       Hash[pairs]
     end
 
-    # Get the current user’s top artists based on calculated affinity. Requires the *user-top-read* scope.
+    # Get the current user's top artists based on calculated affinity. Requires the *user-top-read* scope.
     #
     # @param limit  [Integer] Optional. The number of entities to return. Default: 20. Minimum: 1. Maximum: 50.
     # @param offset [Integer] Optional. The index of the first entity to return. Default: 0 (i.e., the first track). Use with limit to get the next set of entities.
@@ -428,11 +433,13 @@ module RSpotify
     def top_artists(limit: 20, offset: 0, time_range: 'medium_term')
       url = "me/top/artists?limit=#{limit}&offset=#{offset}&time_range=#{time_range}"
       response = User.oauth_get(@id, url)
+
       return response if RSpotify.raw_response
+
       response['items'].map { |i| Artist.new i }
     end
 
-    # Get the current user’s top tracks based on calculated affinity. Requires the *user-top-read* scope.
+    # Get the current user's top tracks based on calculated affinity. Requires the *user-top-read* scope.
     #
     # @param limit  [Integer] Optional. The number of entities to return. Default: 20. Minimum: 1. Maximum: 50.
     # @param offset [Integer] Optional. The index of the first entity to return. Default: 0 (i.e., the first track). Use with limit to get the next set of entities.
@@ -446,7 +453,9 @@ module RSpotify
     def top_tracks(limit: 20, offset: 0, time_range: 'medium_term')
       url = "me/top/tracks?limit=#{limit}&offset=#{offset}&time_range=#{time_range}"
       response = User.oauth_get(@id, url)
+
       return response if RSpotify.raw_response
+
       response['items'].map { |i| Track.new i }
     end
 
@@ -465,7 +474,7 @@ module RSpotify
     #           playlist = RSpotify::Playlist.search('Movie').first
     #           user.unfollow(playlist)
     def unfollow(unfollowed)
-      if unfollowed.is_a? Array
+      if unfollowed.is_a?(Array)
         ids = unfollowed.map(&:id).join(',')
         type = unfollowed.first.type
       else
@@ -474,10 +483,10 @@ module RSpotify
       end
 
       url = if type == 'playlist'
-        "users/#{unfollowed.owner.id}/playlists/#{unfollowed.id}/followers"
-      else
-        "me/following?type=#{type}&ids=#{ids}"
-      end
+              "users/#{unfollowed.owner.id}/playlists/#{unfollowed.id}/followers"
+            else
+              "me/following?type=#{type}&ids=#{ids}"
+            end
 
       User.oauth_delete(@id, url)
       unfollowed
@@ -491,10 +500,11 @@ module RSpotify
     #           devices = user.devices
     #           devices.first.id #=> "5fbb3ba6aa454b5534c4ba43a8c7e8e45a63ad0e"
     def devices
-      url = "me/player/devices"
+      url = 'me/player/devices'
       response = RSpotify.resolve_auth_request(@id, url)
 
       return response if RSpotify.raw_response
+
       response['devices'].map { |i| Device.new i }
     end
   end
