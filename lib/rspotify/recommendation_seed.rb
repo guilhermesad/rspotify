c970916f4af87e211d095162ded7d364e8b4c31d
Deuteu <Deuteu@users.noreply.github.com>
2018-10-11 23:27:36 +0200

    Apply syntax fix to RecommendationSeed

--- lib/rspotify/recommendation_seed.rb
+++ lib/rspotify/recommendation_seed.rb
@@ -1,10 +1,8 @@
 module RSpotify
-
   # @attr [Integer] after_filtering_size The number of tracks available after min_* and max_* filters have been applied.
   # @attr [Integer] after_relinking_size The number of tracks available after relinking for regional availability.
   # @attr [Integer] initial_pool_size    The number of recommended tracks available for this seed.
   class RecommendationSeed < Base
-    
     def initialize(options = {})
       @after_filtering_size = options['afterFilteringSize']
       @after_relinking_size = options['afterRelinkingSize']
@@ -12,7 +10,5 @@ module RSpotify
 
       super(options)
     end
-
   end
-
 end
