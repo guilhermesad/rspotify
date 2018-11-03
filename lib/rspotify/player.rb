09283f0665d85cde7c49e0394c01b424f6262dc9
Deuteu <Deuteu@users.noreply.github.com>
2018-11-04 00:57:59 +0100

    Apply syntax fix in Player.

--- lib/rspotify/player.rb
+++ lib/rspotify/player.rb
@@ -1,6 +1,12 @@
 module RSpotify
+  # @attr [User]    user
+  # @attr [-]       repeat_state
+  # @attr [-]       shuffle_state
+  # @attr [-]       progress
+  # @attr [Boolean] is_playing
+  # @attr [Track]   track
+  # @attr [Device]  device
   class Player < Base
-
     def initialize(user, options = {})
       @user = user
 
@@ -9,13 +15,8 @@ module RSpotify
       @progress      = options['progress_ms']
       @is_playing    = options['is_playing']
 
-      @track = if options['track']
-                 Track.new options['track']
-               end
-
-      @device = if options['device']
-                  Device.new options['device']
-                end
+      @track = Track.new(options['track']) if options['track']
+      @device = Device.new(options['device']) if options['device']
     end
 
     def playing?
@@ -28,7 +29,7 @@ module RSpotify
     # @example
     #           player = user.player
     #           player.play_context(nil,"spotify:album:1Je1IMUlBXcx1Fz0WE7oPT")
-    def play_context(device_id=nil, uri)
+    def play_context(device_id = nil, uri)
       params = {"context_uri": uri}
       play(device_id, params)
     end
@@ -40,19 +41,19 @@ module RSpotify
     #           player = user.player
     #           tracks_uris = ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh", "spotify:track:1301WleyT98MSxVHPZCA6M"]
     #           player.play_tracks(nil, tracks_uris)
-    def play_tracks(device_id=nil, uris)
+    def play_tracks(device_id = nil, uris)
       params = {"uris": uris}
       play(device_id, params)
     end
 
     # Allow browser to trigger playback in the user's currently active spotify app.
-     # If `device_id` is not passed, the currently active spotify app will be triggered
+    # If `device_id` is not passed, the currently active spotify app will be triggered
     #
     # @example
     #           player = user.player
     #           player.play_track(nil, "spotify:track:4iV5W9uYEdYUVa79Axb7Rh")
     #           # User must be a premium subscriber for this feature to work.
-    def play_track(device_id=nil, uri)
+    def play_track(device_id = nil, uri)
       params = {"uris": [uri]}
       play(device_id, params)
     end
@@ -64,8 +65,8 @@ module RSpotify
     #           player = user.player
     #           player.play
     def play(device_id = nil, params = {})
-      url = "me/player/play"
-      url = device_id.nil? ? url : url+"?device_id=#{device_id}"
+      url = 'me/player/play'
+      url = device_id.nil? ? url : "#{url}?device_id=#{device_id}"
 
       User.oauth_put(@user.id, url, params.to_json)
     end
@@ -82,8 +83,8 @@ module RSpotify
     # @example
     #          player = user.player
     #          player.repeat(state: 'track')
-    def repeat(device_id: nil, state: "context")
-      url = "me/player/repeat"
+    def repeat(device_id: nil, state: 'context')
+      url = 'me/player/repeat'
       url += "?state=#{state}"
       url += "&device_id=#{device_id}" if device_id
 
@@ -113,14 +114,14 @@ module RSpotify
     #          player = user.player
     #          player.shuffle(state: false)
     def shuffle(device_id: nil, state: true)
-      url = "me/player/shuffle"
+      url = 'me/player/shuffle'
       url += "?state=#{state}"
       url += "&device_id=#{device_id}" if device_id
 
       User.oauth_put(@user.id, url, {})
     end
 
-    # Skip User’s Playback To Next Track
+    # Skip User's Playback To Next Track
     #
     # @example
     #           player = user.player
@@ -130,7 +131,7 @@ module RSpotify
       User.oauth_post(@user.id, url, {})
     end
 
-    # Skip User’s Playback To Previous Track
+    # Skip User's Playback To Previous Track
     #
     # @example
     #           player = user.player
@@ -151,10 +152,11 @@ module RSpotify
     end
 
     def currently_playing
-      url = "me/player/currently-playing"
+      url = 'me/player/currently-playing'
       response = RSpotify.resolve_auth_request(@user.id, url)
       return response if RSpotify.raw_response
-      Track.new response["item"]
+
+      Track.new response['item']
     end
 
     def seek(position_ms)
