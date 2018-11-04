bf79ea7255340134b525268f71f9426ad3d75897
Deuteu <Deuteu@users.noreply.github.com>
2018-11-04 01:03:49 +0100

    Apply syntax fix in AudioFeatures.

--- lib/rspotify/audio_features.rb
+++ lib/rspotify/audio_features.rb
@@ -1,12 +1,11 @@
 module RSpotify
-
   # @attr [Float]   acousticness      A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.
   # @attr [String]  analysis_url      An HTTP URL to access the full audio analysis of this track. This URL is cryptographically signed and configured to expire after roughly 10 minutes. Do not store these URLs for later use.
   # @attr [Float]   danceability      Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.
   # @attr [Integer] duration_ms       The duration of the track in milliseconds.
   # @attr [Float]   energy            Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.
   # @attr [Float]   instrumentalness  Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.
-  # @attr [Integer] key               The key the track is in. Integers map to pitches using standard {https://en.wikipedia.org/wiki/Pitch_class Pitch Class notation}. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on.
+  # @attr [Integer] key               The key the track is in. Integers map to pitches using standard {https://en.wikipedia.org/wiki/Pitch_class Pitch Class notation}. E.g. 0 = C, 1 = C#/Db, 2 = D, and so on.
   # @attr [Float]   liveness          Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.
   # @attr [Float]   loudness          The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typical range between -60 and 0 db.
   # @attr [Integer] mode              Mode indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived. Major is represented by 1 and minor is 0.
@@ -16,7 +15,6 @@ module RSpotify
   # @attr [String]  track_href        A link to the Web API endpoint providing full details of the track.
   # @attr [Float]   valence           A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
   class AudioFeatures < Base
-
     # Retrieves AudioFeatures object(s) for the track id(s) provided
     #
     # @param ids [String, Array] Either a single track id or a list track ids. Maximum: 100 IDs.
@@ -67,6 +65,5 @@ module RSpotify
       warn 'Spotify API does not support search for audio features'
       false
     end
-
   end
 end
