c05e86aa88593425498e294246ebdfeceef2e5dc
Deuteu <Deuteu@users.noreply.github.com>
2019-03-20 22:41:16 +0100

    Apply syntax fix in OmniAuth:Spotify.

--- lib/rspotify/oauth.rb
+++ lib/rspotify/oauth.rb
@@ -5,7 +5,10 @@ module OmniAuth
     class Spotify < OmniAuth::Strategies::OAuth2
       option :name, 'spotify'
 
-      option :client_options, site: RSpotify::API_URI, authorize_url: RSpotify::AUTHORIZE_URI, token_url: RSpotify::TOKEN_URI
+      option :client_options,
+             site: RSpotify::API_URI,
+             authorize_url: RSpotify::AUTHORIZE_URI,
+             token_url: RSpotify::TOKEN_URI
 
       uid { raw_info['id'] }
 
