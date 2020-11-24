243a3d8e30d81cc84d7c0958d9bc96f284090544
EagleOne33 <gontran.magnat@gmail.com>
2020-11-24 13:35:47 +0100

    add of queue function (#214)
    * add of queue function

--- lib/rspotify/player.rb
+++ lib/rspotify/player.rb
@@ -73,6 +73,21 @@ module RSpotify
       User.oauth_put(@user.id, url, params.to_json)
     end
 
+    # Add an item to the end of the user’s current playback queue
+    # If `device_id` is not passed, the currently active spotify app will be triggered
+    # 
+    # @param [String] device_id the ID of the device to set the repeat state on.
+    # @param [String] uri       the spotify uri of the track to be queued
+    #
+    # @example
+    #           player = user.player
+    #           player.queue("spotify:track:4iV5W9uYEdYUVa79Axb7Rh")
+    def queue(device_id = nil, uri)
+      url = "me/player/queue?uri=#{uri}"
+      url = device_id.nil? ? url : "#{url}&device_id=#{device_id}"
+      User.oauth_post(@user.id, url, {})
+    end
+
     # Toggle the current user's player repeat status.
     # If `device_id` is not passed, the currently active spotify app will be triggered.
     # If `state` is not passed, the currently active context will be set to repeat.
